79ab67eca67607f0f6459207725fa8894e171de1
Dmitriy Ivliev <rootkit89@gmail.com>
2017-05-03 14:40:32 +0400

    experiments with websockets

--- lib/wsdirector.rb
+++ lib/wsdirector.rb
@@ -1,5 +1,37 @@
 require "wsdirector/version"
+require "websocket-client-simple"
+require "wsdirector/echoserver"
 
 module Wsdirector
-  # Your code goes here...
+  def self.call
+    EM::run{
+      Wsdirector::EchoServer.start
+
+      msgs = ['foo','bar','baz']
+
+      EM::add_timer 1 do
+        WebSocket::Client::Simple.connect(Wsdirector::EchoServer.url) do |ws|
+          ws.on :open do
+            puts "connect!"
+            msgs.each do |m|
+              ws.send m
+            end
+          end
+
+          ws.on :message do |msg|
+            puts msg.data
+          end
+
+          EM::add_timer 3 do
+            ws.close
+            EM::stop_event_loop
+          end
+
+          ws.on :close do
+            EM::stop_event_loop
+          end
+        end
+      end
+    }
+  end
 end
